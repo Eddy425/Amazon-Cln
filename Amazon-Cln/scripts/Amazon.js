38b6a81b478118e7abe696fe9a5ec6efcfdd1e44
Aditya Satyarthi <aditya.satyarthi06@gmail.com>
2026-06-25 01:48:07 +0530

    Product Id Usage

--- Amazon-Cln/scripts/Amazon.js
+++ Amazon-Cln/scripts/Amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -55,11 +55,23 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-cart').forEach((button)=> {
   button.addEventListener('click',() => {
-      const productName = button.dataset.productName;
-      cart.push({
-        productName: productName,
-        quantity: 1
+      const productId = button.dataset.productId;
+      let matchingItem;
+
+      cart.forEach((item)=> {
+        if (productId === item.productId){
+          matchingItem = item;
+        }
       });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
       console.log(cart);
   });
 });
