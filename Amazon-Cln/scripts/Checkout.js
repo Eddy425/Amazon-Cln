17306557b3b950d0da8b310bb603dcc4d25723c5
Aditya Satyarthi <aditya.satyarthi06@gmail.com>
2026-06-26 15:12:07 +0530

    Removal of HTML element on click of delete

--- Amazon-Cln/scripts/Checkout.js
+++ Amazon-Cln/scripts/Checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
   const formattedPrice = `₹${matchingProduct.price}`;
 
   cartHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -94,5 +94,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
